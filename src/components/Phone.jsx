8e1bf3117e70ceb6df25b4a54f921a1b3a322617
unknown <oluwatomiwaakinbode@gmail.com>
2022-05-30 09:13:05 +0100

    Add existing file

--- src/components/Phone.jsx
+++ src/components/Phone.jsx
@@ -6,18 +6,29 @@ function Phone() {
   return (
     <>
       <div className="container text-start  text-white bg-primary p-5">
-        <h4 className="">Download the ABC app for easy booking</h4>
-        <p className="fw-light text-break">
+        <h4 className="text-break" style={{}}>
+          Download the ABC app for easy booking.
+        </h4>
+        <p className="fw-light" style={{ maxWidth: '400px' }}>
           Conveniently manage your bookings, tickets, plan all your trips and
           enjoy discounts. ABC Transport App will help you buy tickets more
-          easily
+          easily.
         </p>
-
-        <div className="col">
-          <img src={play} className="w-25 " alt="" style={{ width: '120px' }} />
+        <div className="col-sm-6">
+          <img
+            src={play}
+            className="w-25 "
+            alt=""
+            style={{ width: '200px', marginLeft: '40px' }}
+          />
         </div>
-        <div className="col">
-          <img src={app} className="" alt="" style={{ width: '200px' }} />
+        <div className="col-sm-6">
+          <img
+            src={app}
+            className=""
+            alt=""
+            style={{ width: '200px', marginTop: '-40px' }}
+          />
         </div>
       </div>
     </>
